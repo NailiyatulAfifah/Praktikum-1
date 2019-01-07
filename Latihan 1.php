d099a947a6999320651a5eb124cafd04a5830e34
NailiyatulAfifah <nailiyatul_afifah_26rpl@student.smktelkom-mlg.sch.id>
2019-01-07 15:20:46 +0700

    selesai

---
+++ Latihan 1.php
@@ -0,0 +1,5 @@
+<?php
+  echo "Namaku Nailiyatul Afifah";
+  echo "<br>";
+  echo "Aku sedang belajar PHP di SMK Telkom Malang";
+ ?>
---
+++ Latihan 4.php
@@ -0,0 +1,9 @@
+<?php
+  $nama = "Nailiyatul Afifah";
+  function tampil_nama()
+  {
+    global $nama;
+    echo "Nama Saya : ".$nama;
+  }
+  tampil_nama();
+ ?>
